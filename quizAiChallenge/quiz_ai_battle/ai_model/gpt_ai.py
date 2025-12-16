fa04aa9d1fb5c0cea640b6d48ac1d8fe8bee08ec
Nguyen-Hoang-Ky-Anh <23130010@st.hcmuaf.edu.vn>
2025-12-16 15:56:19 +0700

    UPDATE: update gpt_ai.py

--- quizAiChallenge/quiz_ai_battle/ai_model/gpt_ai.py
+++ quizAiChallenge/quiz_ai_battle/ai_model/gpt_ai.py
@@ -1,36 +1,43 @@
 import os
-from openai import OpenAI
+from groq import Groq
 from .BaseAI import BaseAI
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 class GPTAI(BaseAI):
     def get_answer(self, question):
+        api_key = os.getenv("GROQ_API_KEY")
+
+        if not api_key:
+            raise RuntimeError("GROQ_API_KEY not loaded")
+
+        client = Groq(api_key=api_key)
+
         prompt = f"""
-        You are a quiz AI.
-        Choose the correct answer.
+You are a quiz AI.
+Choose the correct answer.
 
-        Question:
-        {question.content}
+Question:
+{question.content}
 
-        A. {question.option_a}
-        B. {question.option_b}
-        C. {question.option_c}
-        D. {question.option_d}
+A. {question.option_a}
+B. {question.option_b}
+C. {question.option_c}
+D. {question.option_d}
 
-        Respond with ONLY one letter: A, B, C, or D.
-        """
+Respond with ONLY one letter: A, B, C, or D.
+"""
+
         response = client.chat.completions.create(
-            model="gpt-4.1-mini",
+            model="llama-3.1-8b-instant",
             messages=[
-                {"role": "system", "content": "You are smart quiz player."},
+                {"role": "system", "content": "You are a smart quiz player."},
                 {"role": "user", "content": prompt},
             ],
-            temperature=0.2,
+            temperature=0.7,
         )
 
         answer = response.choices[0].message.content.strip().upper()
 
         if answer not in ["A", "B", "C", "D"]:
             return "A"
+
         return answer
