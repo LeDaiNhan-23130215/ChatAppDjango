94a51e9d5363c3d3c4d4dd78d2a5ad33737b4c11
LeDaiNhan-23130215 <23130215@st.hcmuaf.edu.vn>
2026-01-23 17:02:42 +0700

    UPDATE: API ngrok connect, update question generator

--- quizAiChallenge/question_generator/urls.py
+++ quizAiChallenge/question_generator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path("generate/", views.request_ai_questions),
-    path("receive/", views.receive_ai_questions),]
+    path('request-quiz/', views.request_ai_questions, name='request_ai_questions'),
+    path('receive/', csrf_exempt(views.receive_ai_questions), name='receive_ai_questions'),  # chỉ 'receive/'
+]
